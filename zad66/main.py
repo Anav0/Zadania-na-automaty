ac9b415d350abf312dcd1227287d0ed46bb69ab2
Igor Motyka <igor_motyka@mail.com>
2020-06-09 17:14:28 +0200

    better file structure

--- zad66/main.py
+++ zad66/main.py
@@ -1,219 +1,36 @@
-import copy
 import sys
 from loger import Loger
+from dfa import MinimalistDFA
+from hopcroft import HopcroftAlghorithm
+from moore import MooreAlghorithm
+from symbols import acceptingStateChar, startStateChar, noTransitionChar
 import argparse
-
-acceptingStateChar = "'"
-startStateChar = "_"
-noTransitionChar = "-"
-
-class MinimalistDFA:
-    startingState = 0
-    finalStates = []
-    transitionTable = []
-    entriesTable = []
-
-    def __init__(self, startingState, finalStates, transitionTable):
-        self.startingState = startingState
-        self.finalStates = finalStates
-        self.transitionTable = transitionTable
-
-    def prepereEntriesTable(self):
-        N = len(self.transitionTable[0])
-        M = len(self.transitionTable)
-        self.entriesTable = res = [ [ None for o in range(N) ] for j in range(M) ]
-        for i, transition in enumerate(self.transitionTable):
-            for k, enteredState in enumerate(transition):
-                if enteredState is not noTransitionChar:
-                    entries = self.entriesTable[int(enteredState)][k]
-                    if entries:
-                        shouldAdd = True
-                        for existing in entries:
-                            if existing is i:
-                                shouldAdd = False
-                        if shouldAdd:
-                                self.entriesTable[int(enteredState)][k].append(i)
-                    else:
-                        self.entriesTable[int(enteredState)][k] = [i]
-        return self.entriesTable
-
-class HopcroftAlghorithm:
-
-    def __init__(self,logPath):
-        self.loger = Loger(logPath)
-        self.loger.clear()
-
-    def minimize(self, automata):
-        L = []
-        P = [automata.finalStates]
-        entries = automata.prepereEntriesTable()
-
-        # initialize P and L
-        regularStates = []
-        for i, state in enumerate(automata.transitionTable):
-            if i not in automata.finalStates:
-                regularStates.append(i)
-        P.append(regularStates)
-
-        # put smaller list on L
-        toPutOnListL = []
-        if len(P[0]) < len(P[1]):
-            toPutOnListL = ''.join(str(x) for x in P[0])
-        else:
-            toPutOnListL = ''.join(str(x) for x in P[1])
-
-        for i,d in enumerate(automata.transitionTable[0]):
-            L.append('{}{}'.format(toPutOnListL,i))
-
-        # as long as there are elements in L
-        loopCounter = 1;
-        while L:
-            self.loger.log('\n======== {}. loop ========\n'.format(loopCounter))
-            self.loger.log('P: {}'.format(P))
-            self.loger.log('L: {}'.format(L))
-            itemFromL = L.pop(0)
-            states = list(itemFromL)
-            symbol = states.pop()
-            whatEnteres = []
-            for state in states:
-                potentialEntry = automata.entriesTable[int(state)][int(symbol)]
-                if not potentialEntry:
-                    continue
-                else:
-                    whatEnteres += potentialEntry
-
-            # łamanie
-            if not whatEnteres:
-                self.loger.log('Nothing enters {} via symbol: {}'.format(itemFromL[:-1],symbol))
-                loopCounter+=1
-                continue
-            self.loger.log('States: {} are entering {} via symbol: {}'.format(whatEnteres,itemFromL[:-1],symbol))
-            tmpP = copy.deepcopy(P)
-            tmpL = copy.deepcopy(L)
-            self.loger.log('\nLooking for subset \'B\' of P to break apart')
-            for i, B in enumerate(P):
-                splited = splitOff(B, whatEnteres)
-                if len(splited) > 1:
-                    self.loger.log('\n{}. B: {} broken by: {} to: {}'.format(i+1,B,whatEnteres,splited))
-                    tmpP.remove(B)
-                    for split in splited:
-                        tmpP.append(split)
-                    self.loger.log('P after breaking up: {}'.format(tmpP))
-                    # Uzupełnienie list L
-                    flattenB = ''.join(str(x) for x in B)
-                    wasFoundInL = False
-                    self.loger.log('L before filling: {}'.format(L))
-                    for g, element in enumerate(L):
-                        states = list(element)
-                        symbol = states.pop()
-                        joinedStates = "".join(states)
-
-                        if flattenB in joinedStates:
-                            replaced = L.pop(g)
-                            for split in splited:
-                                joinedSplit = ''.join(str(x) for x in split)
-                                self.loger.log('Replaced {} with {} in L'.format(replaced,joinedSplit+symbol))
-                                L.insert(g,joinedSplit+symbol)
-                            wasFoundInL = True
-
-                    if not wasFoundInL:
-                        splited.sort(reverse=True)
-                        smallerBreak = splited[0]
-                        smallerBreak = ''.join(str(x) for x in smallerBreak)
-                        for i, transition in enumerate(automata.transitionTable[0]):
-                            L.append(smallerBreak+str(i))
-                            self.loger.log('Added {} to L'.format(smallerBreak+str(i)))
-                    self.loger.log('L after filling: {}'.format(L))
-                else:
-                    self.loger.log('\n{}. B: {} cannot be broken by {}'.format(i+1,B,whatEnteres))
-            P = tmpP
-            loopCounter+=1
-        self.loger.log("\n")
-        self.loger.log('Final P: {}'.format(P))
-        automataPrint = self.getAutomataPrintOut(P,automata)
-        return automataPrint;
-
-    def getAutomataPrintOut(self,P,automata):
-        printOut = "\t"
-        helperSet = {noTransitionChar: noTransitionChar}
-        for i, symbol in enumerate(automata.transitionTable[0]):
-            printOut+=str(i)+"\t"
-        printOut+="\n"
-        # iterating two times just to get proper prit out may be costly
-        for subset in P:
-            gluedStates = ''.join(str(x) for x in subset);
-            for state in subset:
-                helperSet[str(state)] = gluedStates
-        for subset in P:
-            addStartStateMarker = False
-            addFinishStateMarker = False
-            for state in subset:
-                printOut+=str(state)
-                if state in automata.finalStates:
-                    addFinishStateMarker = True
-                if state is automata.startingState:
-                    addStartStateMarker = True
-
-            printOut+=acceptingStateChar if addFinishStateMarker else ""
-            printOut+=startStateChar if addStartStateMarker else ""
-            printOut+="\t"
-            printOut+='\t'.join(helperSet[x] for x in automata.transitionTable[subset[0]]);
-            printOut+="\n"
-        self.loger.log("\nMinimized automata transition table:\n")
-        self.loger.log(printOut)
-        return printOut
-
-def minimalistDFAfromFile(filePath):
-    startingState = 0
-    finalStates = []
-    transitionTable = []
-    with open(filePath,'r') as file:
-        index = 0
-        for line in file:
-            if index is 0:
-                index+=1
-                continue
-            splitedLine = line.strip().split("\t")
-            if startStateChar in splitedLine[0]:
-                startingState = index-1
-            if acceptingStateChar in splitedLine[0]:
-                finalStates.append(index-1)
-            transition = []
-            for i, char in enumerate(splitedLine):
-                if i is 0: continue
-                transition.append(splitedLine[i])
-            transitionTable.append(transition)
-            index+=1
-
-        return MinimalistDFA(startingState,finalStates,transitionTable)
-
-def splitOff(array,toSplit):
-    a = []
-    b = []
-    for value in array:
-        if value in toSplit:
-            b.append(value)
-        else:
-            a.append(value)
-    if b and a:
-        return [a,b]
-    else:
-        return [a]
+from helpers import minimalistDFAfromFile
 
 def main():
-    parser = argparse.ArgumentParser(description='Minimizes automata from file using Hopcroft alghorithm')
+    parser = argparse.ArgumentParser(description='Minimizes automata from file using Hopcroft or Moore alghorithm')
     parser.add_argument('filePath', metavar='f', help='path to transition table of DFA')
+    parser.add_argument('alghorithm', metavar='a', help='alghorithm name to use: moore or hopcroft')
     parser.add_argument('--logPath', metavar='l', help='path to store log file')
     args = parser.parse_args()
 
     # W pliku log.txt jest szczegółowy opis kroków jakie podejmuje algorytm
     logPath = sys.argv[0]
     fileName = sys.argv[1]
+    alghorithmName = sys.argv[2].lower()
+    print(fileName)
     if logPath:
         logPath = 'zad66/log.txt'
     automata = minimalistDFAfromFile(fileName)
-    alghorithm = HopcroftAlghorithm(logPath)
-    print(alghorithm.minimize(automata))
+    hopcroft = HopcroftAlghorithm(logPath)
+
+    if alghorithmName is 'moore':
+        moore = MooreAlghorithm(logPath)
+        print(moore.minimize(automata))
+    elif alghorithmName is 'hopcroft':
+        print(hopcroft.minimize(automata))
+    else:
+        print(hopcroft.minimize(automata))
 
 if __name__ == "__main__":
     main()
